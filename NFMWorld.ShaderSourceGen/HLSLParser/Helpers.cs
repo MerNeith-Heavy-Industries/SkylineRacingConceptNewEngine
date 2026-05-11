24f622dfd617eeaa2de735ce12a846fb5c641b6a
uwx <maxine@cybergal.com>
2026-05-11 04:41:15 +0200

    shadersourcegen

---
+++ NFMWorld.ShaderSourceGen/HLSLParser/Helpers.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace NFMWorld.ShaderSourceGen;
+
+internal static class Helpers
+{
+    extension(string)
+    {
+        public static unsafe string FromSpan(ReadOnlySpan<char> span)
+        {
+            fixed (char* ptr = span)
+            {
+                return new string(ptr, 0, span.Length);
+            }
+
+            return null;
+        }
+    }
+
+    extension(int)
+    {
+        public static int Parse(ReadOnlySpan<char> s, IFormatProvider provider)
+        {
+            return int.Parse(string.FromSpan(s), provider);
+        }
+    }
+}
