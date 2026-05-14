dd9229150ddf595ae1f17d0e94abc2b6bee66e3b
uwx <maxine@cybergal.com>
2026-05-14 04:20:37 +0200

    extra stuff for SRC

---
+++ NFMWorld.Library/Mad/Rad/SrcRad3dCollisionMesh.cs
@@ -0,0 +1,7 @@
+﻿using MessagePack;
+
+namespace NFMWorldLibrary.Rad;
+
+[MessagePackObject]
+[method: SerializationConstructor]
+public readonly record struct SrcRad3dCollisionMesh([property: Key(0)] Vector3[] Vertices, [property: Key(1)] ushort[] Indices);
