107480a2f897fe6f3901c01ab2dee0ed948a7ef0
uwx <maxine@cybergal.com>
2026-06-02 12:27:04 +0200

    fix aabb generation & calculation

--- NFMWorld.Library/Mad/Rad/SrcRad3dCollisionMesh.cs
+++ NFMWorld.Library/Mad/Rad/SrcRad3dCollisionMesh.cs
@@ -10,17 +10,18 @@ public readonly record struct SrcRad3dCollisionMesh([property: Key(0)] f64Vector
 {
     [IgnoreMember] public (f64Vector3 min, f64Vector3 max)[] Aabb { get; } = CalculateAabb(Vertices, Indices);
 
-    private static (f64Vector3 min, f64Vector3 max)[] CalculateAabb(f64Vector3[] vertices, ushort[] indices)
+    private static (f64Vector3 min, f64Vector3 max)[] CalculateAabb(ReadOnlySpan<f64Vector3> vertices, ReadOnlySpan<ushort> indices)
     {
-        var arr = new (f64Vector3 min, f64Vector3 max)[vertices.Length / 3];
-        for (int i = 0; i < vertices.Length; i += 3)
+        var aabbs = new (f64Vector3 min, f64Vector3 max)[indices.Length / 3];
+        for (var i = 0; i < indices.Length; i += 3)
         {
-            ref readonly var v0 = ref vertices[i];
-            ref readonly var v1 = ref vertices[i + 1];
-            ref readonly var v2 = ref vertices[i + 2];
+            ref readonly var v0 = ref vertices[indices[i]];
+            ref readonly var v1 = ref vertices[indices[i + 1]];
+            ref readonly var v2 = ref vertices[indices[i + 2]];
 
-            arr[i / 3] = TriangleMesh.ComputeAABB(v0, v1, v2);
+            aabbs[i / 3] = TriangleMesh.ComputeAABB(v0, v1, v2);
         }
-        return arr;
+
+        return aabbs;
     }
 }
