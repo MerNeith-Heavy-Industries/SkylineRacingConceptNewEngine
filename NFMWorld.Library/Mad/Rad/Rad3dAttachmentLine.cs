f5303afb2b1e76eb0faf45bfb5270d44bfe41a2a
uwx <maxine@cybergal.com>
2026-06-07 18:06:40 +0200

    fix missing msgpack attr
    (cherry picked from commit 032897fb146ce7234302fd3c2907a90774129a9a)

--- NFMWorld.Library/Mad/Rad/Rad3dAttachmentLine.cs
+++ NFMWorld.Library/Mad/Rad/Rad3dAttachmentLine.cs
@@ -1,3 +1,6 @@
-﻿namespace NFMWorldLibrary.Rad;
+﻿using MessagePack;
 
-public readonly record struct Rad3dAttachmentLine(AttachmentLineDirection Direction, fix64 Offset);
+namespace NFMWorldLibrary.Rad;
+
+[MessagePackObject]
+public readonly record struct Rad3dAttachmentLine([property: Key(0)] AttachmentLineDirection Direction, [property: Key(1)] fix64 Offset);
