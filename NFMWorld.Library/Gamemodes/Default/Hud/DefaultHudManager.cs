9ef5a433be8bb624b1873396268aceb4e3258e5d
uwx <maxine@cybergal.com>
2026-06-10 00:04:43 +0200

    forgot to assign datacontext

--- NFMWorld.Library/Gamemodes/Default/Hud/DefaultHudManager.cs
+++ NFMWorld.Library/Gamemodes/Default/Hud/DefaultHudManager.cs
@@ -10,11 +10,21 @@ public class DefaultHudManager : IHud
 {
     private readonly FlexPanel _rootPanel = new();
     private readonly OverlayPanel _overlay = new();
-    public HudViewModel DataContext { get; set; } = new();
+
+    public HudViewModel DataContext
+    {
+        get;
+        set
+        {
+            field = value;
+            _rootPanel.DataContext = field;
+        }
+    } = new();
     public required FocusManager FocusManager { get; set; }
 
     public DefaultHudManager()
     {
+        _rootPanel.DataContext = DataContext;
         _rootPanel.Children.Add(_overlay);
         
         _overlay.ContentChildren.Add(new PowerDamageBars());
