36f5bf324abf847c1082342827a838023468dd78
uwx <maxine@cybergal.com>
2026-06-22 19:44:13 +0200

    OverlayPanel is component

--- NFMWorld.Reactor/OverlayPanel.cs
+++ NFMWorld.Reactor/OverlayPanel.cs
@@ -1,118 +1,21 @@
-using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Numerics;
-using WorldXaml.UI.Metadata;
-using ObservableCollections;
-using WorldXaml.ObservableCollections;
-using WorldXaml.UI.Base;
+using System.Linq;
+using static WorldXaml.UI.Yoga.Nodes;
+using Yoga;
 
-namespace WorldXaml.UI.Yoga;
+namespace NFMWorld.Reactor;
 
 /// <summary>
-/// A panel that renders its children with absolute positioning, as if they were the only child, disregarding each
-/// other's layout.
+/// A component that renders its children with absolute positioning overlaid
+/// on a relative-positioned container. Equivalent to a CSS overlay panel.
 /// </summary>
-public class OverlayPanel : Visual
+public class OverlayPanel(params VNode[] children) : Component
 {
-    private readonly FlexPanel _overlayContainer;
-
-    public OverlayPanel()
+    protected override VNode Render()
     {
-        _overlayContainer = new FlexPanel
-        {
-            Flex = 1,
-            Position = YgPositionType.Relative, // establishes containing block for absolute children
-        };
-        
-        ContentChildren.CollectionChanged += ContentChildrenChanged;
+        var wrapped = children
+            .Select(c => FlexPanel(position: YgPositionType.Absolute, children: c))
+            .ToArray();
+        return FlexPanel(flex: 1, position: YgPositionType.Relative, children: wrapped);
     }
-
-    private void ContentChildrenChanged(in NotifyCollectionChangedEventArgs<Node> e)
-    {
-        switch (e.Action)
-        {
-            case NotifyCollectionChangedAction.Add:
-                if (e.IsSingleItem)
-                {
-                    e.NewItem.Position = YgPositionType.Absolute;
-                    _overlayContainer.Children.Add(e.NewItem);
-                }
-                else
-                {
-                    foreach (var item in e.NewItems)
-                    {
-                        item.Position = YgPositionType.Absolute;
-                        _overlayContainer.Children.Add(item);
-                    }
-                }
-                break;
-            case NotifyCollectionChangedAction.Remove:
-                if (e.IsSingleItem)
-                    _overlayContainer.Children.Remove(e.OldItem);
-                else
-                    foreach (var item in e.OldItems)
-                        _overlayContainer.Children.Remove(item);
-                break;
-            case NotifyCollectionChangedAction.Replace:
-                if (e.IsSingleItem)
-                {
-                    var oldItem = e.OldItem;
-                    var newItem = e.NewItem;
-                    var idx = _overlayContainer.Children.IndexOf(oldItem);
-                    if (idx >= 0)
-                    {
-                        _overlayContainer.Children.RemoveAt(idx);
-                        newItem.Position = YgPositionType.Absolute;
-                        _overlayContainer.Children.Insert(idx, newItem);
-                    }
-                }
-                else
-                {
-                    for (var i = 0; i < e.NewItems.Length; i++)
-                    {
-                        var oldItem = e.OldItems[i];
-                        var newItem = e.NewItems[i];
-                        var idx = _overlayContainer.Children.IndexOf(oldItem);
-                        if (idx >= 0)
-                        {
-                            _overlayContainer.Children.RemoveAt(idx);
-                            newItem.Position = YgPositionType.Absolute;
-                            _overlayContainer.Children.Insert(idx, newItem);
-                        }
-                    }
-                }
-                break;
-            case NotifyCollectionChangedAction.Move:
-                // Absolute positioning means visual/tree order is irrelevant.
-                break;
-            case NotifyCollectionChangedAction.Reset:
-                _overlayContainer.Children.Clear();
-                foreach (var item in ContentChildren)
-                {
-                    item.Position = YgPositionType.Absolute;
-                    _overlayContainer.Children.Add(item);
-                }
-                break;
-            default:
-                throw new ArgumentOutOfRangeException(nameof(e.Action), e.Action, null);
-        }
-    }
-
-    internal override YGNodePtr Contents => _overlayContainer.NodeInternal;
-    public override IReadOnlyList<Visual> VisualChildren => [_overlayContainer];
-    
-    public override Vector2 FocusOrigin => Vector2.Zero;
-    public override Vector2 FocusSize => Vector2.Zero;
-
-    public override bool CanHaveChildren => true;
-    public override void AddChild(Visual child) => ContentChildren.Add((Node)child);
-    public override void InsertAt(int index, Visual child) => ContentChildren.Insert(index, (Node)child);
-    public override void RemoveAt(int index) => ContentChildren.RemoveAt(index);
-
-    /// <summary>
-    /// Children supplied by the user of this control. These are injected into the internal
-    /// <see cref="FlexPanel"/> with absolute positioning.
-    /// </summary>
-    [Content]
-    public NonSynchronizedObservableList<Node> ContentChildren { get; } = new();
 }
+
