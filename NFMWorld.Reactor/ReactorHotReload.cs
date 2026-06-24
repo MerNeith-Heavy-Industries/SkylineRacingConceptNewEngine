4d43023dc274aae67011f9d221fe3d3cc78dc72d
uwx <maxine@cybergal.com>
2026-06-25 01:18:49 +0200

    reactor hot reload

---
+++ NFMWorld.Reactor/ReactorHotReload.cs
@@ -0,0 +1,29 @@
+﻿#if DEBUG
+using WorldXaml.UI.Base;
+
+[assembly: System.Reflection.Metadata.MetadataUpdateHandlerAttribute(typeof(NFMWorld.Reactor.HotReloadService))]
+
+namespace NFMWorld.Reactor;
+
+public static class HotReloadService
+{
+    public static event Action<Type[]?>? UpdateApplicationEvent;
+
+    internal static void ClearCache(Type[]? types)
+    {
+        Logging.Debug("Hot Reload Service event - ClearCache");
+    }
+
+    internal static void UpdateApplication(Type[]? types)
+    {
+        UpdateApplicationEvent?.Invoke(types);
+
+        Logging.Debug("Hot Reload Service event - UpdateApplication");
+    }
+}
+#else
+public static class HotReloadService
+{
+    public static event Action<Type[]?>? UpdateApplicationEvent;
+}
+#endif
