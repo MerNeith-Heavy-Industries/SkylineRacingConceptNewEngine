1d96a22dc7e7bd659ca0dfa68b84682a6011c57c
uwx <maxine@cybergal.com>
2026-06-25 00:05:13 +0200

    fixs for new generator
    (cherry picked from commit 096fe19b47ac11014c2f0a871c78417eb611a25e)

--- NFMWorld.Library/Gamemodes/Default/Hud/HudHost.cs
+++ NFMWorld.Library/Gamemodes/Default/Hud/HudHost.cs
@@ -1,5 +1,4 @@
 using NFMWorld.Reactor;
-using NFMWorldLibrary.Backend.Gamemodes;
 using static WorldXaml.UI.Yoga.Nodes;
 using static NFMWorldLibrary.DriverInterface.UI.Elements.Nodes;
 
@@ -9,11 +8,21 @@ namespace NFMWorld.UI.Hud;
 /// Root HUD component. Receives state, provides it via context, and renders
 /// all HUD sub-components. Instantiated by <see cref="DefaultHudManager"/>.
 /// </summary>
-public class HudHost(HudState state, params VNode[] children) : Component
+public class HUDHost : Component
 {
+    private readonly HudState _state;
+
+    public HUDHost(HudState state)
+    {
+        _state = state;
+    }
+
     protected override VNode Render()
     {
-        ProvideContext(HUDContexts.Hud, state);
-        return View(children: children);
+        ProvideContext(HUDContexts.Hud, _state);
+        return View(children: [
+            CentralTextView(),
+            PowerDamageBars()
+        ]);
     }
 }
