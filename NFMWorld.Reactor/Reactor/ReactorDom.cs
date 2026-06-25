6722416889e538514660db6f8b1037bf6c91ed7f
uwx <maxine@cybergal.com>
2026-06-25 04:58:30 +0200

    add reactordom

---
+++ NFMWorld.Reactor/Reactor/ReactorDom.cs
@@ -0,0 +1,40 @@
+﻿namespace NFMWorld.Reactor;
+
+public class ReactorDom
+{
+    private Reconciler _reconciler = new();
+    private VNode? _rootNode;
+    private Visual? _container;
+
+    public ReactorDom()
+    {
+        HotReloadService.UpdateApplicationEvent += OnHotReloadServiceOnUpdateApplicationEvent;
+    }
+
+    ~ReactorDom()
+    {
+        HotReloadService.UpdateApplicationEvent -= OnHotReloadServiceOnUpdateApplicationEvent;
+    }
+
+    private void OnHotReloadServiceOnUpdateApplicationEvent(Type[]? types)
+    {
+        Update();
+    }
+
+    public Visual? Root { get; private set; }
+
+    public void Mount(Visual container, VNode vnode)
+    {
+        _container = container;
+        _rootNode = vnode;
+        Update();
+    }
+
+    private void Update()
+    {
+        if (_rootNode != null && _container != null)
+        {
+            Root = _reconciler.Reconcile(_rootNode, _container, Root);
+        }
+    }
+}
