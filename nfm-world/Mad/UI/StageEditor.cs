cbf98329ac0e6d4ff1a4cbb82b058720d5f80a7c
uwx <maxine@cybergal.com>
2026-07-05 05:06:01 +0200

    add ClientStageRenderer.DetectChanges for faster client stage diffing when backend stage changes
    (cherry picked from commit 779793f899b3e7bd77500910c5e6b07587ab3ab7)

--- nfm-world/Mad/UI/StageEditor.cs
+++ nfm-world/Mad/UI/StageEditor.cs
@@ -1373,7 +1373,10 @@ public class StageEditorPhase : BasePhase
     {
         if (ActiveTab?.Stage == null) return;
 
-        ActiveTab.StageRenderer = new ClientStageRenderer(_graphicsDevice, ActiveTab.Stage);
+        if (ActiveTab.StageRenderer == null)
+            ActiveTab.StageRenderer = new ClientStageRenderer(_graphicsDevice, ActiveTab.Stage);
+        else
+            ActiveTab.StageRenderer.DetectChanges(true);
         
         // ClientStageRenderer.ctor calls World.ResetValues(), re-apply our tab settings
         ApplyTabWorldValuesToWorld();
