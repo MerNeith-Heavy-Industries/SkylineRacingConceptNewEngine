8ae635300dbc524885b170c6cb191e81152261ea
uwx <maxine@cybergal.com>
2026-07-10 21:51:05 +0200

    fix leaking CollisionDebugMesh
    (cherry picked from commit fb17d02ad2111f5b062035665e61d7c59c2e8863)

--- nfm-world/Mad/GameObject/StageObjectGameObject.cs
+++ nfm-world/Mad/GameObject/StageObjectGameObject.cs
@@ -1,19 +1,22 @@
-﻿using NFMWorldLibrary;
+﻿using System;
+using NFMWorldLibrary;
 using NFMWorldLibrary.Backend;
 
 namespace NFMWorld;
 
-public class StageObjectGameObject : MeshedGameObject
+public class StageObjectGameObject : MeshedGameObject, IDisposable
 {
     private readonly StageObject _obj;
+    private readonly CollisionDebugMesh? _collisionDebug;
 
     public StageObjectGameObject(Mesh mesh, StageObject obj) : base(mesh, obj.Position, obj.Rotation)
     {
         _obj = obj;
-        Children = [new CollisionDebugMesh(obj.Boxes)
+        _collisionDebug = new CollisionDebugMesh(obj.Boxes)
         {
             Parent = this
-        }];
+        };
+        Children = [_collisionDebug];
     }
 
     public override void GameTick(IStage? stage = null)
@@ -21,5 +24,11 @@ public class StageObjectGameObject : MeshedGameObject
         base.GameTick(stage);
         Position = _obj.Position;
         Rotation = _obj.Rotation;
+    }
+
+    public void Dispose()
+    {
+        _collisionDebug?.Dispose();
+        GC.SuppressFinalize(this);
     }
 }
