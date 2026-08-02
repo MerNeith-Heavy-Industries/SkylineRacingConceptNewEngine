79098a6242a826101947129c6c0df3269b40bdec
uwx <maxine@cybergal.com>
2026-08-02 06:01:59 +0200

    cleaner rewrites of cef stuff

--- NFMWorld.BrowserProcess/NfmwCefApp.cs
+++ NFMWorld.BrowserProcess/NfmwCefApp.cs
@@ -3,21 +3,6 @@ using Xilium.CefGlue.Common.Shared;
 
 namespace NFMWorld.UI.Cef;
 
-internal abstract class CommonCefApp : CefApp
-{
-    private readonly CustomScheme[] _customSchemes;
-
-    internal CommonCefApp(CustomScheme[] customSchemes = null) => this._customSchemes = customSchemes;
-
-    protected override void OnRegisterCustomSchemes(CefSchemeRegistrar registrar)
-    {
-        if (this._customSchemes == null)
-            return;
-        foreach (CustomScheme customScheme in this._customSchemes)
-            registrar.AddCustomScheme(customScheme.SchemeName, customScheme.Options);
-    }
-}
-
 /// <summary>
 /// Minimal CefApp implementation. Handles subprocess detection and provides
 /// the render process handler for V8 JavaScript context setup.
@@ -34,16 +19,5 @@ internal sealed class NfmwCefApp : CommonCefApp
     protected override CefRenderProcessHandler GetRenderProcessHandler()
     {
         return _renderProcessHandler;
-    }
-
-    protected override void OnBeforeCommandLineProcessing(string processType, CefCommandLine commandLine)
-    {
-        // Reduce CEF's background process overhead
-        commandLine.AppendSwitch("--disable-background-networking");
-        commandLine.AppendSwitch("--disable-sync");
-        commandLine.AppendSwitch("--disable-extensions");
-        commandLine.AppendSwitch("--disable-plugins");
-
-        base.OnBeforeCommandLineProcessing(processType, commandLine);
     }
 }
