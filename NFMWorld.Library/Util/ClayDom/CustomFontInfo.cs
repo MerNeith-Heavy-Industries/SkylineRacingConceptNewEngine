ab14cb7bf21840095a3b2698ecc1a2b6fb9b207c
uwx <maxine@cybergal.com>
2026-08-19 08:30:54 +0200

    split off CustomFontInfo

---
+++ NFMWorld.Library/Util/ClayDom/CustomFontInfo.cs
@@ -0,0 +1,9 @@
+﻿using NFMWorld.DriverInterface.DriverInterface;
+
+namespace NFMWorld.ClayDom;
+
+public class CustomFontInfo
+{
+    public string fontFamily = "";
+    public FontStyle fontStyle = FontStyle.Plain;
+}
