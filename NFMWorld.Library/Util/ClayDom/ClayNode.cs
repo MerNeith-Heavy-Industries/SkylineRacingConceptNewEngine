6ef25d111752c4cf9fbed52859343b34501b5e53
uwx <maxine@cybergal.com>
2026-08-19 12:20:11 +0200

    split claydom into files

---
+++ NFMWorld.Library/Util/ClayDom/ClayNode.cs
@@ -0,0 +1,10 @@
+﻿using nfm_world_library.Lua;
+
+namespace NFMWorld.ClayDom;
+
+[LuaVisible]
+public abstract partial class ClayNode
+{
+    [LuaName]
+    public abstract NodeType NodeType { get; }
+}
